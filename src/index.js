9733b283ef072d29e3d309770966ffe37f3bbce7
Mateusz Brzozowski <neertoon@icloud.com>
2021-01-10 14:16:22 +0100

    Virtual Dom - podpinanie elementu na stronę

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ const element = <h1>Hello World</h1>;
 
 //To co znajduje siê tutaj to element znajduj¹cy siê w Virtual DOM
 console.log(element);
+
+// A teraz wyl¹duije na stronie
+ReactDom.render(element, document.getElementById('root'));
